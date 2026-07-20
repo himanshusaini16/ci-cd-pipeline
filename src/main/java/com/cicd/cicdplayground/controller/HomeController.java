0cafd8d39be2596a22899b141157065e41d34064
Himanshu Saini <himanshusaini025@gmail.com>
2026-07-20 17:19:07 +0530

    userAddPointAdded

--- src/main/java/com/cicd/cicdplayground/controller/HomeController.java
+++ src/main/java/com/cicd/cicdplayground/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class HomeController {
@@ -18,5 +20,13 @@ public class HomeController {
     @GetMapping("/workflow")
     ResponseEntity<String> workflow() {
         return ResponseEntity.ok("Welcome to the Workflow Page!");
+    }
+
+    @GetMapping("/user")
+    ResponseEntity<Map<String,Object>> getUserInfo() {
+        Map<String, Object> userInfo = Map.of(
+                "name", "John Doe",
+                "email", "jone123@gmail.com");
+        return ResponseEntity.ok(userInfo);
     }
 }
